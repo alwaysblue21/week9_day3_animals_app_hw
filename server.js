66a17625d0d584f43361bf1320c520d4b040f41e
Je Yun <alwaysblue21@gmail.com>
2023-06-26 16:04:51 -0400

    show.ejs

--- server.js
+++ server.js
@@ -4,23 +4,45 @@ const morgan = require("morgan");
 const Animal = require("./models/animals")
 const app = express();
 
-app.use(morgan("dev"));
 
+//middle
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }))
+
+//routes
 app.get("/", (req, res) => {
     res.send("Hello World!")
 });
 
+//index
 app.get("/animals", async (req, res) => {
     const allAnimals = await Animal.find({});
 
     res.render("index.ejs", { animals: allAnimals })
 })
 
-
+//new
 app.get("/animals/new", (req, res) => {
     res.render("new.ejs")
 })
 
+//create?
+app.post("/animals", async (req, res) => {
+    if(req.body.extinct === "on"){
+        req.body.extinct = true;
+    } else {
+        req.body.extinct = false;
+    }
+    await Animal.create(req.body)
+    res.redirect("/animals")
+})
+
+app.get("/animals/:id", async (req, res) => {
+    const foundAnimal = await Animal.findById(req.params.id)
+    res.render("show.ejs", { animal: foundAnimal})
+})
+
+//listen
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
