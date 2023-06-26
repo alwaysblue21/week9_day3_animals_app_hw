857e947b8f9eca2712c19e4213a048ddf7ddab91
Je Yun <alwaysblue21@gmail.com>
2023-06-26 16:47:27 -0400

    edit and update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
-//routes
+//--routes
 app.get("/", (req, res) => {
     res.send("Hello World!")
 });
@@ -28,7 +28,7 @@ app.get("/animals/new", (req, res) => {
     res.render("new.ejs")
 })
 
-//create?
+//create
 app.post("/animals", async (req, res) => {
     if(req.body.extinct === "on"){
         req.body.extinct = true;
@@ -49,6 +49,24 @@ app.get("/animals/:id", async (req, res) => {
 //delete
 app.delete("/animals/:id", async (req, res) => {
     await Animal.findByIdAndDelete(req.params.id)
+    res.redirect("/animals")
+})
+
+//edit
+app.get("/animals/:id/edit", async (req, res) => {
+    const animal = await Animal.findById(req.params.id)
+    res.render("edit.ejs", { animal })
+})
+
+//update
+app.put("/animals/:id", async (req, res) => {
+    if(req.body.extinct === "on") {
+        req.body.extinct = true;
+    } else {
+        req.body.extinct = false;
+    }
+
+    await Animal.findByIdAndUpdate(req.params.id, req.body)
     res.redirect("/animals")
 })
 
